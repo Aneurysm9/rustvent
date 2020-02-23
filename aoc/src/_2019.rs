8b3338f0748073f17407e93d3e17b810b9229e9b
Anthony J Mirabella <a9@aneurysm9.com>
2020-02-23 02:53:47 -0500

    another cargo fmt issue?

--- aoc/src/_2019.rs
+++ aoc/src/_2019.rs
@@ -3,8 +3,8 @@ use crate::Solution;
 pub mod day1;
 
 pub fn new(day: &str, input: String) -> Option<impl Solution> {
-	match day {
-		"1" => Some(day1::Runner { input }),
-		_ => None,
-	}
+    match day {
+        "1" => Some(day1::Runner { input }),
+        _ => None,
+    }
 }
